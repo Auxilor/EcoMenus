6ae3854d007010f84af4b319816a83457152f6ac
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-19 14:09:49 +0000

    Enhance reload command feedback with execution time and menu count

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecomenus/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecomenus/commands/CommandReload.kt
@@ -1,6 +1,9 @@
 package com.willfp.ecomenus.commands
 
 import com.willfp.eco.core.command.impl.Subcommand
+import com.willfp.eco.util.StringUtils
+import com.willfp.eco.util.toNiceString
+import com.willfp.ecomenus.menus.EcoMenus
 import com.willfp.ecomenus.plugin
 import org.bukkit.command.CommandSender
 
@@ -12,6 +15,10 @@ object CommandReload : Subcommand(
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
         plugin.reload()
-        sender.sendMessage(plugin.langYml.getMessage("reloaded"))
+        sender.sendMessage(
+            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                .replace("%time%", plugin.reloadWithTime().toNiceString())
+                .replace("%count%", EcoMenus.values().size.toString())
+        )
     }
 }
