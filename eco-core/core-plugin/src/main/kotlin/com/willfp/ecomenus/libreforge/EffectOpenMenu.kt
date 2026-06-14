106f06887b1a90c4fbca9c1f5fe3b87a66dd225a
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-06-14 09:17:04 +0100

    Add wiki v2 documentation metadata

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecomenus/libreforge/EffectOpenMenu.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecomenus/libreforge/EffectOpenMenu.kt
@@ -3,6 +3,7 @@ package com.willfp.ecomenus.libreforge
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.eco.util.openMenu
 import com.willfp.ecomenus.menus.EcoMenus
+import com.willfp.libreforge.ArgType
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Effect
@@ -10,12 +11,21 @@ import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
 
 object EffectOpenMenu : Effect<NoCompileData>("open_menu") {
+    override val description = "Opens a menu for the player."
+
+    override val categories = setOf("meta")
+
     override val parameters = setOf(
         TriggerParameter.PLAYER
     )
 
     override val arguments = arguments {
-        require("menu", "You must specify the menu ID!")
+        require(
+            "menu",
+            "You must specify the menu ID!",
+            description = "The ID of the menu to open for the player.",
+            type = ArgType.STRING
+        )
     }
 
     override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecomenus/libreforge/EffectResetPreviousMenu.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecomenus/libreforge/EffectResetPreviousMenu.kt
@@ -9,6 +9,11 @@ import com.willfp.libreforge.triggers.TriggerData
 import com.willfp.libreforge.triggers.TriggerParameter
 
 object EffectResetPreviousMenu : Effect<NoCompileData>("reset_previous_menu") {
+    override val description = "Clears the player's history of previously opened menus, so the back button " +
+        "will no longer return to them."
+
+    override val categories = setOf("meta")
+
     override val parameters = setOf(
         TriggerParameter.PLAYER
     )
